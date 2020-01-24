96c72c07440b03ce0b22d4d2d8fe15ddb0f7722a
Urumbur <adrian.madry94@gmail.com>
2020-01-24 13:51:44 +0100

    Updates

---
+++ precache-manifest.96c37162a4eb27f01240678815761fbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74753b0075ec74f196f4f8d4a93e3ea5",
+    "url": "/weather_app/index.html"
+  },
+  {
+    "revision": "a52b40c7e24658e5faf1",
+    "url": "/weather_app/static/css/main.7c3cc73e.chunk.css"
+  },
+  {
+    "revision": "f4e5f53f3b50406afc5e",
+    "url": "/weather_app/static/js/2.26890984.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/weather_app/static/js/2.26890984.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a52b40c7e24658e5faf1",
+    "url": "/weather_app/static/js/main.dac5be72.chunk.js"
+  },
+  {
+    "revision": "800ef261b4fb05346cd3",
+    "url": "/weather_app/static/js/runtime-main.bf27409f.js"
+  }
+]);
